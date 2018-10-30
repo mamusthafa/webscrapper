f210322e3ad8338e7475b072c797486af617e157
Musthafa MA <ma.musthafa6@gmail.com>
2018-10-30 21:38:19 +0530

    Update webscrap.py

--- webscrap.py
+++ webscrap.py
@@ -1,9 +1,12 @@
 from bs4 import BeautifulSoup
 import requests
 import bs4
+#insert the link which you want to scrap from
 data = requests.get('https://www.flipkart.com/search?q=coffee&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off')
 soup = bs4.BeautifulSoup(data.text,'html')
+#inside braket mention the class name
 for i in soup.select('._2cLu-l'):
+    #print the desired item/tag
     print(i.text)
 
 
